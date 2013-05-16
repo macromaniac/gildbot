f947a59e8ad413de6ce5519996a423f28b34b3ce
macromaniac <joeycaero@gmail.com>
2013-05-16 00:57:44 -0700

    modified:   GildedComment.py

--- GildedComment.py
+++ GildedComment.py
@@ -9,16 +9,19 @@ class GildedComment:
 		self.myId = data.id
 		self.timeMade = data.created_utc
 		self.perma = data.permalink
+	def getID(self):
+		return self.myID
 	def getAge(self):
 		return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(self.timeMade))
 	def getDay(self):
 		return time.strftime('%Y-%m-%d', time.localtime(self.timeMade))
 	def getAgeSeconds(self):
+		print( time.mktime(time.gmtime()))
 		return time.mktime(time.gmtime()) - self.timeMade
 	def getPerma(self):
 		return self.perma + '?context=1337'
 	def getAgeHours(self):
-		time.strftime('%H', time.gmtime(self.getAgeSeconds()))
+		return str( int( round( self.getAgeSeconds() / (60 * 60 ) ) ) )
 	def getTitle(self):
 		return str(self.getAgeHours()) + 'h - in r/' +  self.sub + ' by u/' + self.author
 		
