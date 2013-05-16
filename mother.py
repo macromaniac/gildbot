fdc49cbdbe77f404db838b6a23f2604557b4d817
macromaniac <joeycaero@gmail.com>
2013-05-16 00:59:56 -0700

    modified:   mother.py

--- mother.py
+++ mother.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import config
 import time
@@ -5,7 +6,9 @@ import praw
 from GildedComment import GildedComment
 
 # haven't done python in centuries, so this will be interesting ;)
-r = praw.Reddit(user_agent = 'Bot that looks through gilded comments, finds comments above 3 gilds, and then submits them to r/' + subreddit + ', code @ https://github.com/macromaniac/gildbot')
+r = praw.Reddit(user_agent = 'User-Agent: Bot that looks through gilded comments, finds comments above 3 gilds, and then submits them to r/' + config.subreddit + ', by /u/macromaniac')
+
+submittedDict = {}
 
 def getStr( thing ):
 	return str(thing.gilded) + ' ' + thing.subreddit.display_name + ' ' + str(thing.author.name) + ' ' +  thing.id + ' ' + str(thing.created_utc) + ' '
@@ -14,15 +17,25 @@ def getDay():
 	return time.strftime('%Y-%m-%d', time.localtime())
 
 def submitLink( gildedComment ):
-	try:
-		r.submit( config.subreddit, gildedComment.getTitle() , url = gildedComment.getPerma()  )
-		print("submitted link!")
-	except praw.errors.AlreadySubmitted:
-		#if they comment was already submitted, ignore it
-		pass
+	#the dictionary is a crude protection against reposts. If the program gets restarted it will try
+	#to resubmit all the links for the day and then fail. Not a big deal. An easy improvement would be
+	#to use persistant data
+	if !(gildedComment.getID in submittedDict):
+		submittedDict[gildedComment.getID] = 1
+		try:
+			print('trying to submit to r/' + str(config.subreddit) + ' title: ' + str(gildedComment.getTitle()) + ' url: ' + str(gildedComment.getPerma()))
+			r.submit( config.subreddit, gildedComment.getTitle() , url = gildedComment.getPerma()  )
+			print("submitted link!")
+		except praw.errors.AlreadySubmitted:
+			#if they comment was already submitted, ignore it
+			pass
+		except praw.errors.NotLoggedIn:
+			print("Bot got logged off!")
+			sys.exit(1)
 
 def parseComments():
 	comments = r.get_comments('all', gilded_only = True, limit = None)
+	already_done = []
 	for comment in comments:
 		if comment.gilded >= config.threshold:
 			#get the gilded comment
@@ -34,13 +47,16 @@ def parseComments():
 def mainLoop():
 	while True:
 		parseComments()
-		time.sleep(pollrate)
+		time.sleep(config.pollrate)
 
 def init():
 	print('Starting Bot...')
-	r.login( config.username, config.password)
+	try:
+		r.login( config.username, config.password)
+	except praw.errors.NotLoggedIn:
+		print("Bot couldn't log in, exiting")
+		sys.exit(1)
 	print('Bot logged in, Parsing comments...')
-	print('today is: ' + str(getDay()))
 	mainLoop()
 
 init()
